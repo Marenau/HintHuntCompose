c41009f167ea452c625de6e9adc43ff0b5fd9be4
Marenau <malin.vadimka@mail.ru>
2024-05-19 02:37:32 +0300

    Add dynamic theme

--- app/src/main/java/com/corylab/hinthunt/ui/activity/MainActivity.kt
+++ app/src/main/java/com/corylab/hinthunt/ui/activity/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.corylab.hinthunt.ui.activity
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.res.Configuration
+import android.database.ContentObserver
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.isSystemInDarkTheme
 import com.corylab.hinthunt.ui.application.HintHunt
 import com.corylab.hinthunt.ui.navgraph.Navigation
 import com.corylab.hinthunt.ui.theme.HintHuntComposeTheme
@@ -18,21 +28,38 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private lateinit var themeObserver: ThemeObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val theme = viewModel.getInt("theme")
-        AppCompatDelegate.setDefaultNightMode(
-            when (theme) {
-                0 -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-                1 -> AppCompatDelegate.MODE_NIGHT_NO
-                2 -> AppCompatDelegate.MODE_NIGHT_YES
-                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-            }
+        val handler = Handler(Looper.getMainLooper())
+        themeObserver = ThemeObserver(this, handler)
+        contentResolver.registerContentObserver(
+            Settings.System.getUriFor(Settings.System.ACCELEROMETER_ROTATION),
+            false,
+            themeObserver
         )
         setContent {
-            HintHuntComposeTheme(darkTheme = theme == 2) {
+            HintHuntComposeTheme(darkTheme = isSystemInDarkTheme()) {
                 Navigation(applicationContext)
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentResolver.unregisterContentObserver(themeObserver)
+    }
+}
+
+class ThemeObserver(private val context: Context, handler: Handler) : ContentObserver(handler) {
+    override fun onChange(selfChange: Boolean) {
+        val nightMode = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        val newNightMode = when (nightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> AppCompatDelegate.MODE_NIGHT_NO
+            Configuration.UI_MODE_NIGHT_YES -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
+        AppCompatDelegate.setDefaultNightMode(newNightMode)
     }
 }
